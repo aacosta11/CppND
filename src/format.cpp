cb2d76fdf0044a71c2a70bd59f9e5e2767be4c99
aacosta11 <acostaadrian2020@gmail.com>
2023-01-31 19:15:49 -0800

    string formatting function

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,40 @@
-#include <string>
-
 #include "format.h"
+
+#include <string>
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+// format seconds to HH:MM:SS
+string Format::ElapsedTime(long seconds) {
+  string output("");
+  int hours{0}, minutes{0};
+
+  // find hours
+  while (seconds - 3600 > 0) {
+    seconds -= 3600;
+    hours++;
+  }
+  // find minutes
+  while (seconds - 60 > 0) {
+    seconds -= 60;
+    minutes++;
+  }
+
+  // add hours to string
+  if (hours < 10) {
+    output += "0";
+  }
+  output += std::to_string(hours) + ":";
+  // add minutes to string
+  if (minutes < 10) {
+    output += "0";
+  }
+  output += std::to_string(minutes) + ":";
+  // add seconds to string
+  if (seconds < 10) {
+    output += "0";
+  }
+  output += std::to_string(seconds);
+
+  return output;
+}
